37f8d96a27426a931e37cb147be981d1d51119ec
Abhijeeth Padarthi <rkinabhi@gmail.com>
2019-03-28 12:40:58 +0530

    Update app_constants.py

--- common/constants/app_constants.py
+++ common/constants/app_constants.py
@@ -6,6 +6,5 @@ FORWARD_DAYS = [1, 3, 5]
 CLASSIFIERS = {
     "RF": RandomForestClassifier(n_estimators=100, max_depth=10),
     "GBDT": GradientBoostingClassifier(n_estimators=100, max_depth=10, loss="exponential")
+    "HYBRID": HybridClassifier(rf_params = {'n_estimators':100, 'max_depth':10}, gbdt_params = {'n_estimators':100, 'max_depth':10, 'loss':'exponential'})
 }
-# TODO - add this classifier to the current list of classifiers
-XYZ = HybridClassifier()
